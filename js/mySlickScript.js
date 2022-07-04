aea32762d8d1dabbe68209c63c32ff5408bc526a
andTheowind <59265638+andTheowind@users.noreply.github.com>
2022-07-04 21:51:01 +0300

    Add files via upload

---
+++ js/mySlickScript.js
@@ -0,0 +1,14 @@
+$(document).on('ready', function() {
+            
+          $(".slider-promo-imgs .slider").slick({
+            dots: true,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows:false,
+            centerPadding: '120px',
+            autoplay: true,
+            autoplaySpeed: 2000,
+          });
+        
+});
